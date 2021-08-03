b333e9260ddac9993a720d6de12b6490a9429f8d
Kleber Guedes Rodrigues <kleber.guedes-rodrigues@amido.com>
2021-08-03 17:35:36 +0100

    Changed environment variables names

--- src/api/xxAMIDOxx.xxSTACKSxx.Worker/ItemChangedEvent.cs
+++ src/api/xxAMIDOxx.xxSTACKSxx.Worker/ItemChangedEvent.cs
@@ -1,21 +1,22 @@
-using System;
+﻿using System;
 using Amido.Stacks.Application.CQRS.ApplicationEvents;
-using Amido.Stacks.Core.Operations;
 
 namespace xxAMIDOxx.xxSTACKSxx.Worker
 {
     public class ItemChangedEvent : IApplicationEvent
     {
-        public ItemChangedEvent(int operationCode, Guid correlationId)
+        public ItemChangedEvent(int operationCode, Guid correlationId, string id, string eTag)
         {
             OperationCode = operationCode;
             CorrelationId = correlationId;
+            Id = id;
+            ETag = eTag;
         }
 
         public int EventCode => 999;
-
         public int OperationCode { get; private set; }
-
         public Guid CorrelationId { get; private set; }
+        public string Id { get; private set; }
+        public string ETag { get; private set; }
     }
 }
