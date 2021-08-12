0808c1ba6964c9af3d44a04e2338cc05ed939fd0
Kleber Guedes <56262054+kleber-amido@users.noreply.github.com>
2021-08-12 08:47:04 +0100

    Added unit tests to functions (#15)
    * Add unit tests to Listener
    
    * Add sln files to functions
    
    * Ignoring local settings

--- src/functions/xxAMIDOxx.xxSTACKSxx.Listener/StacksListener.cs
+++ src/functions/xxAMIDOxx.xxSTACKSxx.Listener/StacksListener.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Text;
-using Amido.Stacks.Application.CQRS.Events;
+﻿using Amido.Stacks.Application.CQRS.Events;
 using Amido.Stacks.Messaging.Azure.ServiceBus.Serializers;
 using Microsoft.Azure.ServiceBus;
 using Microsoft.Azure.WebJobs;
@@ -28,7 +26,7 @@ namespace xxAMIDOxx.xxSTACKSxx.Listener
             var appEvent = msgReader.Read<StacksCloudEvent<MenuCreatedEvent>>(mySbMsg);
 
             // TODO: work with appEvent
-            logger.LogInformation($"Message read. Menu Id: {appEvent.Data.MenuId}");
+            logger.LogInformation($"Message read. Menu Id: {appEvent?.Data?.MenuId}");
 
             logger.LogInformation($"C# ServiceBus topic trigger function processed message: {appEvent}");
         }
