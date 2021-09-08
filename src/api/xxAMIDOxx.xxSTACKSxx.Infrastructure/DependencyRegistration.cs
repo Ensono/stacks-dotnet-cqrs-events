99814aaee3bf45eceedd4d495813f53b99846ec6
scottvaughanamido <scott.vaughan@amido.com>
2021-09-08 13:51:10 +0100

    Add event hub publisher. Configure EventPublisher parameter in templates.

--- src/api/xxAMIDOxx.xxSTACKSxx.Infrastructure/DependencyRegistration.cs
+++ src/api/xxAMIDOxx.xxSTACKSxx.Infrastructure/DependencyRegistration.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using Amido.Stacks.Application.CQRS.ApplicationEvents;
 using Amido.Stacks.Application.CQRS.Commands;
 using Amido.Stacks.Application.CQRS.Queries;
@@ -6,7 +6,6 @@ using Amido.Stacks.Configuration.Extensions;
 using Amido.Stacks.Data.Documents.CosmosDB;
 using Amido.Stacks.Data.Documents.CosmosDB.Extensions;
 using Amido.Stacks.DependencyInjection;
-using Amido.Stacks.Messaging.Azure.ServiceBus.Configuration;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
@@ -45,12 +44,19 @@ namespace xxAMIDOxx.xxSTACKSxx.Infrastructure
             services.AddSecrets();
             services.AddCosmosDB();
 
-            //TODO: Evaluate if event publishers should be generic, probably not, EventHandler are generic tough
-            // AddEventPublishers(context, services);
-
-            // Azure Service Bus
-            services.Configure<ServiceBusConfiguration>(context.Configuration.GetSection("ServiceBusConfiguration"));
+#if (EventPublisherServiceBus)
+            services.Configure<Amido.Stacks.Messaging.Azure.ServiceBus.Configuration.ServiceBusConfiguration>(context.Configuration.GetSection("ServiceBusConfiguration"));
             services.AddServiceBus();
+            services.AddTransient<IApplicationEventPublisher, Amido.Stacks.Messaging.Azure.ServiceBus.Senders.Publishers.EventPublisher>();
+#endif
+#if (EventPublisherEventHub)
+            services.Configure<Amido.Stacks.Messaging.Azure.EventHub.Configuration.EventHubConfiguration>(context.Configuration.GetSection("EventHubConfiguration"));
+            services.AddEventHub();
+            services.AddTransient<IApplicationEventPublisher, Amido.Stacks.Messaging.Azure.EventHub.Publisher.EventPublisher>();
+#endif
+#if (EventPublisherNone)
+            services.AddTransient<IApplicationEventPublisher, DummyEventPublisher>();
+#endif
 
             if (Environment.GetEnvironmentVariable("USE_MEMORY_STORAGE") == null)
                 services.AddTransient<IMenuRepository, MenuRepository>();
@@ -84,14 +90,13 @@ namespace xxAMIDOxx.xxSTACKSxx.Infrastructure
             }
         }
 
-        private static void AddEventPublishers(WebHostBuilderContext context, IServiceCollection services)
+        private static void AddEventPublishers(IServiceCollection services)
         {
             log.Information("Loading implementations of {interface}", typeof(IApplicationEventPublisher).Name);
             var definitions = typeof(DummyEventPublisher).Assembly.GetImplementationsOf(typeof(IApplicationEventPublisher));
             foreach (var definition in definitions)
             {
                 log.Information("Registering '{implementation}' as implementation of '{interface}'", definition.implementation.FullName, definition.interfaceVariation.FullName);
-                //TODO: maybe this should be singleton
                 services.AddTransient(definition.interfaceVariation, definition.implementation);
             }
         }
