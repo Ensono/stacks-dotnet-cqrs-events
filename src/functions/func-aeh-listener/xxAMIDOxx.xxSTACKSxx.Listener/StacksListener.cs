f7b601898836780a73711a1f93df005f2d9b2728
scottvaughanamido <scott.vaughan@amido.com>
2021-09-14 13:24:16 +0100

    Add unit test for listener function. Reference message reader from event hub package.

--- src/functions/func-aeh-listener/xxAMIDOxx.xxSTACKSxx.Listener/StacksListener.cs
+++ src/functions/func-aeh-listener/xxAMIDOxx.xxSTACKSxx.Listener/StacksListener.cs
@@ -1,31 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Amido.Stacks.Application.CQRS.Events;
+using Amido.Stacks.Messaging.Azure.EventHub.Serializers;
 using Microsoft.Azure.EventHubs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace xxAMIDOxx.xxSTACKSxx.Listener
 {
-    // TODO: Move this to https://github.com/amido/stacks-dotnet-packages-messaging-aeh
-    public interface IMessageReader
-    {
-        T Read<T>(EventData eventData);
-    }
-
-    // TODO: Move this to https://github.com/amido/stacks-dotnet-packages-messaging-aeh
-    public class JsonMessageSerializer : IMessageReader
-    {
-        public T Read<T>(EventData eventData)
-        {
-            var jsonBody = Encoding.UTF8.GetString(eventData.Body);
-            return JsonConvert.DeserializeObject<T>(jsonBody);
-        }
-    }
-
     public class StacksListener
     {
         private readonly IMessageReader msgReader;
--- src/functions/func-aeh-listener/xxAMIDOxx.xxSTACKSxx.Listener/Startup.cs
+++ src/functions/func-aeh-listener/xxAMIDOxx.xxSTACKSxx.Listener/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+﻿using Amido.Stacks.Messaging.Azure.EventHub.Serializers;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
