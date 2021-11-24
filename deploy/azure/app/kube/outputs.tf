7696461f0ca1c23ad276a6fae0eb86262d8cd06a
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2021-11-24 16:18:22 +0000

    Corrected outputs
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- deploy/azure/app/kube/outputs.tf
+++ deploy/azure/app/kube/outputs.tf
@@ -43,66 +43,66 @@ output "dns_name" {
 
 output "servicebus_namespace" {
   description = "Service bus namespace"
-  value       = module.servicebus.*.servicebus_namespace
+  value       = module.servicebus.*.servicebus_namespace[0]
 }
 
 output "servicebus_topic_name" {
   description = "Name of the topic"
-  value       = module.servicebus.*.servicebus_topic_name
+  value       = module.servicebus.*.servicebus_topic_name[0]
 }
 
 output "servicebus_subscription_name" {
   description = "Servicebus Subscription name"
-  value       = module.servicebus.*.servicebus_subscription_name
+  value       = module.servicebus.*.servicebus_subscription_name[0]
 }
 
 output "servicebus_connectionstring" {
-  value = module.servicebus.*.servicebus_connectionstring
+  value = module.servicebus.*.servicebus_connectionstring[0]
 }
 
 output "servicebus_subscription_filtered_name" {
   description = "Servicebus Subscription filtered name"
-  value       = module.servicebus.*.servicebus_subscription_filtered_name
+  value       = module.servicebus.*.servicebus_subscription_filtered_name[0]
 }
 
 output "servicebus_subscription_id" {
   description = "Servicebus Subscription ID"
-  value       = module.servicebus.*.servicebus_subscription_id
+  value       = module.servicebus.*.servicebus_subscription_id[0]
 }
 
 output "servicebus_subscription_filtered_id" {
   description = "Servicebus Subscription filtered ID"
-  value       = module.servicebus.*.servicebus_subscription_filtered_id
+  value       = module.servicebus.*.servicebus_subscription_filtered_id[0]
 }
 
 output "function_publisher_id" {
-  value = module.servicebus.*.function_publisher_id
+  value = module.servicebus.*.function_publisher_id[0]
 }
 
 output "function_listener_id" {
-  value = module.servicebus.*.function_listener_id
+  value = module.servicebus.*.function_listener_id[0]
 }
 
 output "function_publisher_name" {
-  value = module.servicebus.*.function_publisher_name
+  value = module.servicebus.*.function_publisher_name[0]
 }
 
 output "function_listener_name" {
-  value = module.servicebus.*.function_listener_name
+  value = module.servicebus.*.function_listener_name[0]
 }
 
 output "eventhub_connectionstring" {
-  value = module.eventhub.*.eventhub_connectionstring
+  value = module.eventhub.*.eventhub_connectionstring[0]
 }
 
 output "eventhub_name" {
-  value = module.eventhub.*.eventhub_name
+  value = module.eventhub.*.eventhub_name[0]
 }
 
 output "eventhub_sa_connectionstring" {
-  value = module.eventhub.*.eventhub_sa_connectionstring
+  value = module.eventhub.*.eventhub_sa_connectionstring[0]
 }
 
 output "eventhub_sa_container" {
-  value = module.eventhub.*.eventhub_sa_container
+  value = module.eventhub.*.eventhub_sa_container[0]
 }
