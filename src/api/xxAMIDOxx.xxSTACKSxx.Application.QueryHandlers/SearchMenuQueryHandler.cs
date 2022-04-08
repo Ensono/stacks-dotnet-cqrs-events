9eb37e949eef8200be857be64d87a0d434bceaa3
Ali Russell <ali.russell@amido.com>
2022-04-08 15:51:33 +0100

    AB#4525 <Ali> WIP - Application.QueryHandlers project namesspaces updated to File-scoped namespace declaration

--- src/api/xxAMIDOxx.xxSTACKSxx.Application.QueryHandlers/SearchMenuQueryHandler.cs
+++ src/api/xxAMIDOxx.xxSTACKSxx.Application.QueryHandlers/SearchMenuQueryHandler.cs
@@ -6,66 +6,65 @@ using Amido.Stacks.Data.Documents.Abstractions;
 using xxAMIDOxx.xxSTACKSxx.CQRS.Queries.SearchMenu;
 using xxAMIDOxx.xxSTACKSxx.Domain;
 
-namespace xxAMIDOxx.xxSTACKSxx.Application.QueryHandlers
+namespace xxAMIDOxx.xxSTACKSxx.Application.QueryHandlers;
+
+public class SearchMenuQueryHandler : IQueryHandler<SearchMenu, SearchMenuResult>
 {
-    public class SearchMenuQueryHandler : IQueryHandler<SearchMenu, SearchMenuResult>
+    readonly IDocumentSearch<Menu> storage;
+
+    public SearchMenuQueryHandler(IDocumentSearch<Menu> storage)
     {
-        readonly IDocumentSearch<Menu> storage;
+        this.storage = storage;
+    }
 
-        public SearchMenuQueryHandler(IDocumentSearch<Menu> storage)
-        {
-            this.storage = storage;
-        }
+    public async Task<SearchMenuResult> ExecuteAsync(SearchMenu criteria)
+    {
+        if (criteria == null)
+            throw new ArgumentException("A valid SearchMenuQueryCriteria os required!");
 
-        public async Task<SearchMenuResult> ExecuteAsync(SearchMenu criteria)
-        {
-            if (criteria == null)
-                throw new ArgumentException("A valid SearchMenuQueryCriteria os required!");
+        int pageSize = 10;
+        int pageNumber = 1;
+        var searchTerm = string.Empty;
+        Guid tenantId = criteria.TenantId.HasValue ? criteria.TenantId.Value : Guid.Empty;
 
-            int pageSize = 10;
-            int pageNumber = 1;
-            var searchTerm = string.Empty;
-            Guid tenantId = criteria.TenantId.HasValue ? criteria.TenantId.Value : Guid.Empty;
+        if (criteria.PageSize.HasValue && criteria.PageSize > 0)
+            pageSize = criteria.PageSize.Value;
 
-            if (criteria.PageSize.HasValue && criteria.PageSize > 0)
-                pageSize = criteria.PageSize.Value;
+        if (criteria.PageNumber.HasValue && criteria.PageNumber > 0)
+            pageNumber = criteria.PageNumber.Value;
 
-            if (criteria.PageNumber.HasValue && criteria.PageNumber > 0)
-                pageNumber = criteria.PageNumber.Value;
+        if (!string.IsNullOrEmpty(criteria.SearchText))
+            searchTerm = criteria.SearchText.Trim();
 
-            if (!string.IsNullOrEmpty(criteria.SearchText))
-                searchTerm = criteria.SearchText.Trim();
+        bool restaurantIdProvided = criteria.TenantId.HasValue;
 
-            bool restaurantIdProvided = criteria.TenantId.HasValue;
+        var results = await storage.Search(
+            itemFilter =>
+                (string.IsNullOrEmpty(searchTerm) || itemFilter.Name.Contains(searchTerm)) &&
+                //Nullable types must have a value when passed to a seach, this is why we convert it to non nullable and pass a boolean check
+                (!restaurantIdProvided || itemFilter.TenantId == tenantId)
+            ,
+            null,
+            pageSize,
+            pageNumber);
 
-            var results = await storage.Search(
-                itemFilter =>
-                    (string.IsNullOrEmpty(searchTerm) || itemFilter.Name.Contains(searchTerm)) &&
-                    //Nullable types must have a value when passed to a seach, this is why we convert it to non nullable and pass a boolean check
-                    (!restaurantIdProvided || itemFilter.TenantId == tenantId)
-                    ,
-                null,
-                pageSize,
-                pageNumber);
+        var result = new SearchMenuResult();
+        result.PageSize = pageSize;
+        result.PageNumber = pageNumber;
 
-            var result = new SearchMenuResult();
-            result.PageSize = pageSize;
-            result.PageNumber = pageNumber;
-
-            if (!results.IsSuccessful)
-                return result;
-
-            result.Results = results.Content.Select(i => new SearchMenuResultItem()
-            {
-                Id = i.Id,
-                RestaurantId = i.TenantId,
-                Name = i.Name,
-                Description = i.Description,
-                Enabled = i.Enabled
-            });
-
-
+        if (!results.IsSuccessful)
             return result;
-        }
+
+        result.Results = results.Content.Select(i => new SearchMenuResultItem()
+        {
+            Id = i.Id,
+            RestaurantId = i.TenantId,
+            Name = i.Name,
+            Description = i.Description,
+            Enabled = i.Enabled
+        });
+
+
+        return result;
     }
 }
