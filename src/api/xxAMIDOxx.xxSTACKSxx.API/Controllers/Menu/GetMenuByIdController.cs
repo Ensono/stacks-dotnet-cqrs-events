277a632611ff6c7358efc14344257131f5af21ab
Ali Russell <ali.russell@amido.com>
2022-04-21 11:43:46 +0100

    AB#4543 <Ali> WIP - Add unit test for GetMenuByIdController when Menu is null

--- src/api/xxAMIDOxx.xxSTACKSxx.API/Controllers/Menu/GetMenuByIdController.cs
+++ src/api/xxAMIDOxx.xxSTACKSxx.API/Controllers/Menu/GetMenuByIdController.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 using System.Threading.Tasks;
 using Amido.Stacks.Application.CQRS.Queries;
 using Microsoft.AspNetCore.Authorization;
